dcedd1e927f94119e75bb3968b845710e0fe6e4e
Sabari <Sabari@vaf.ai>
2021-07-25 17:00:58 +0530

    initial commit

---
+++ AI_Form_Reader/Program.cs
@@ -0,0 +1,156 @@
+﻿using Azure;
+using Azure.AI.FormRecognizer;
+using Azure.AI.FormRecognizer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AI_Form_Reader
+{
+    class Program
+    {
+       static string endpoint = "https://menporul.cognitiveservices.azure.com/";
+        static string apiKey = "78ad55b53bf5442eabbb6e987e87047f";
+    
+        static  void Main(string[] args)
+        {
+             var credential = new AzureKeyCredential(apiKey);
+             var client = new FormRecognizerClient(new Uri(endpoint), credential);
+
+
+            string invoicePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Invoice_1.pdf");
+
+            using var stream = new FileStream(invoicePath, FileMode.Open);
+            var options = new RecognizeInvoicesOptions() { Locale = "en-US" };
+
+            RecognizeInvoicesOperation operation =  client.StartRecognizeInvoicesAsync(stream, options).Result;
+            Response<RecognizedFormCollection> operationResponse =  operation.WaitForCompletionAsync().Result;
+            RecognizedFormCollection invoices = operationResponse.Value;
+
+            // To see the list of all the supported fields returned by service and its corresponding types, consult:
+            // https://aka.ms/formrecognizer/invoicefields
+
+            RecognizedForm invoice = invoices.Single();
+
+            if (invoice.Fields.TryGetValue("VendorName", out FormField vendorNameField))
+            {
+                if (vendorNameField.Value.ValueType == FieldValueType.String)
+                {
+                    string vendorName = vendorNameField.Value.AsString();
+                    Console.WriteLine($"Vendor Name: '{vendorName}', with confidence {vendorNameField.Confidence}");
+                }
+            }
+
+            if (invoice.Fields.TryGetValue("CustomerName", out FormField customerNameField))
+            {
+                if (customerNameField.Value.ValueType == FieldValueType.String)
+                {
+                    string customerName = customerNameField.Value.AsString();
+                    Console.WriteLine($"Customer Name: '{customerName}', with confidence {customerNameField.Confidence}");
+                }
+            }
+
+            if (invoice.Fields.TryGetValue("Items", out FormField itemsField))
+            {
+                if (itemsField.Value.ValueType == FieldValueType.List)
+                {
+                    foreach (FormField itemField in itemsField.Value.AsList())
+                    {
+                        Console.WriteLine("Item:");
+
+                        if (itemField.Value.ValueType == FieldValueType.Dictionary)
+                        {
+                            IReadOnlyDictionary<string, FormField> itemFields = itemField.Value.AsDictionary();
+
+                            if (itemFields.TryGetValue("Description", out FormField itemDescriptionField))
+                            {
+                                if (itemDescriptionField.Value.ValueType == FieldValueType.String)
+                                {
+                                    string itemDescription = itemDescriptionField.Value.AsString();
+
+                                    Console.WriteLine($"  Description: '{itemDescription}', with confidence {itemDescriptionField.Confidence}");
+                                }
+                            }
+
+                            if (itemFields.TryGetValue("Quantity", out FormField itemQuantityField))
+                            {
+                                if (itemQuantityField.Value.ValueType == FieldValueType.Float)
+                                {
+                                    float quantityAmount = itemQuantityField.Value.AsFloat();
+
+                                    Console.WriteLine($"  Quantity: '{quantityAmount}', with confidence {itemQuantityField.Confidence}");
+                                }
+                            }
+
+                            if (itemFields.TryGetValue("UnitPrice", out FormField itemUnitPriceField))
+                            {
+                                if (itemUnitPriceField.Value.ValueType == FieldValueType.Float)
+                                {
+                                    float itemUnitPrice = itemUnitPriceField.Value.AsFloat();
+
+                                    Console.WriteLine($"  UnitPrice: '{itemUnitPrice}', with confidence {itemUnitPriceField.Confidence}");
+                                }
+                            }
+
+                            if (itemFields.TryGetValue("Tax", out FormField itemTaxPriceField))
+                            {
+                                if (itemTaxPriceField.Value.ValueType == FieldValueType.Float)
+                                {
+                                    try
+                                    {
+                                        float itemTax = itemTaxPriceField.Value.AsFloat();
+                                        Console.WriteLine($"  Tax: '{itemTax}', with confidence {itemTaxPriceField.Confidence}");
+                                    }
+                                    catch
+                                    {
+                                        string itemTaxText = itemTaxPriceField.ValueData.Text;
+                                        Console.WriteLine($"  Tax: '{itemTaxText}', with confidence {itemTaxPriceField.Confidence}");
+                                    }
+                                }
+                            }
+
+                            if (itemFields.TryGetValue("Amount", out FormField itemAmountField))
+                            {
+                                if (itemAmountField.Value.ValueType == FieldValueType.Float)
+                                {
+                                    float itemAmount = itemAmountField.Value.AsFloat();
+
+                                    Console.WriteLine($"  Amount: '{itemAmount}', with confidence {itemAmountField.Confidence}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (invoice.Fields.TryGetValue("SubTotal", out FormField subTotalField))
+            {
+                if (subTotalField.Value.ValueType == FieldValueType.Float)
+                {
+                    float subTotal = subTotalField.Value.AsFloat();
+                    Console.WriteLine($"Sub Total: '{subTotal}', with confidence {subTotalField.Confidence}");
+                }
+            }
+
+            if (invoice.Fields.TryGetValue("TotalTax", out FormField totalTaxField))
+            {
+                if (totalTaxField.Value.ValueType == FieldValueType.Float)
+                {
+                    float totalTax = totalTaxField.Value.AsFloat();
+                    Console.WriteLine($"Total Tax: '{totalTax}', with confidence {totalTaxField.Confidence}");
+                }
+            }
+
+            if (invoice.Fields.TryGetValue("InvoiceTotal", out FormField invoiceTotalField))
+            {
+                if (invoiceTotalField.Value.ValueType == FieldValueType.Float)
+                {
+                    float invoiceTotal = invoiceTotalField.Value.AsFloat();
+                    Console.WriteLine($"Invoice Total: '{invoiceTotal}', with confidence {invoiceTotalField.Confidence}");
+                }
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AzureAIForms/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using Azure;
+using Azure.AI.FormRecognizer;
+using Azure.AI.FormRecognizer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureAIForms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult InvoiceReader()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UploadInvoice(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+            var filePaths = new List<string>();
+
+
+
+             string endpoint = "https://menporul.cognitiveservices.azure.com/";
+             string apiKey = "78ad55b53bf5442eabbb6e987e87047f";
+
+            var credential = new AzureKeyCredential(apiKey);
+            var client = new FormRecognizerClient(new Uri(endpoint), credential);
+            // full path to file in temp location
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + HttpContext.Request.Form.Files[0].FileName ;
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
+           //we are using Temp file name just for the example. Add your own file path.
+            filePaths.Add(filePath);
+            var stream = new FileStream(filePath, FileMode.Create);
+            
+                foreach (var formFile in HttpContext.Request.Form.Files)
+                {
+                    if (formFile.Length > 0)
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+
+            //string invoicePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Invoice_1.pdf");
+            stream.Close();
+
+            var options = new RecognizeInvoicesOptions() { Locale = "en-US" };
+            // using var stream1 = new FileStream(@"C:\Users\91848\Source\Repos\AI_Form_Reader\AI_Form_Reader\"+ "Invoice_1.pdf", FileMode.Open);
+
+            using var stream1 = new FileStream(filePath, FileMode.Open);
+            RecognizeInvoicesOperation operation = client.StartRecognizeInvoicesAsync(stream1, options).Result;
+            Response<RecognizedFormCollection> operationResponse = operation.WaitForCompletionAsync().Result;
+            RecognizedFormCollection invoices = operationResponse.Value;
+
+          
+
+            RecognizedForm invoice = invoices.Single();
+            ExpandoObject obj = new ExpandoObject();
+            List<KeyValuePair<string, string>> data = new List<KeyValuePair<string, string>>();
+            foreach (var item in invoice.Fields)
+            {
+                obj.TryAdd<string, object>(item.Key,item.Value?.ValueData?.Text);
+                data.Add(new KeyValuePair<string, string>(item.Key, item.Value?.ValueData?.Text));
+            }
+
+            //if (invoice.Fields.TryGetValue("VendorName", out FormField vendorNameField))
+            //{
+            //    if (vendorNameField.Value.ValueType == FieldValueType.String)
+            //    {
+            //        string vendorName = vendorNameField.Value.AsString();
+            //        Console.WriteLine($"Vendor Name: '{vendorName}', with confidence {vendorNameField.Confidence}");
+            //    }
+            //}
+
+            //if (invoice.Fields.TryGetValue("CustomerName", out FormField customerNameField))
+            //{
+            //    if (customerNameField.Value.ValueType == FieldValueType.String)
+            //    {
+            //        string customerName = customerNameField.Value.AsString();
+            //        Console.WriteLine($"Customer Name: '{customerName}', with confidence {customerNameField.Confidence}");
+            //    }
+            //}
+            //foreach (var formFile in files)
+            //{
+            //    if (formFile.Length > 0)
+            //    {
+            //        // full path to file in temp location
+            //        var filePath = Path.GetTempFileName(); //we are using Temp file name just for the example. Add your own file path.
+            //        filePaths.Add(filePath);
+            //        using (var stream = new FileStream(filePath, FileMode.Create))
+            //        {
+            //            await formFile.CopyToAsync(stream);
+            //        }
+            //    }
+            //}
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+            return Ok(data);
+        }
+    }
+}
